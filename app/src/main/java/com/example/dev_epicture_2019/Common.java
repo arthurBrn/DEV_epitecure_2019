41f65bba33694bc6ccb1424be0aedf096414f79b
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-01-31 16:42:34 +0100

    add swipe between activiteis

--- app/src/main/java/com/example/dev_epicture_2019/Common.java
+++ app/src/main/java/com/example/dev_epicture_2019/Common.java
@@ -4,21 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Common extends AppCompatActivity {
 
+    static protected float x1;
+    static protected float x2;
     static private void select_current_activity(BottomNavigationView navigationBar, int index) {
         Menu menu = navigationBar.getMenu();
         MenuItem menuItem = menu.getItem(index);
         menuItem.setChecked(true);
     }
 
-    static private void create_intent(Context mContext, Class C) {
+    static public void create_intent(Context mContext, Class C) {
         Intent intent = new Intent(mContext, C);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(intent);
@@ -43,5 +47,16 @@ public class Common extends AppCompatActivity {
                 }
                 return false;
             });
+    }
+
+    static public void swipe(MotionEvent touchEvent) {
+        switch (touchEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/dev_epicture_2019/Search.java
+++ app/src/main/java/com/example/dev_epicture_2019/Search.java
@@ -1,16 +1,13 @@
 package com.example.dev_epicture_2019;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MotionEvent;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Search extends Common {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +15,15 @@ public class Search extends Common {
         setContentView(R.layout.activity_search);
         BottomNavigationView navigationBar = findViewById(R.id.navigationBar);
         Common.change_activity(navigationBar, 1, getApplicationContext());
-        overridePendingTransition(0,0);
+        overridePendingTransition(0, 0);
+    }
+
+    public boolean onTouchEvent(MotionEvent touchEvent) {
+        swipe(touchEvent);
+        if (x1 < x2)
+            create_intent(getApplicationContext(), Home.class);
+        else
+            create_intent(getApplicationContext(), Add.class);
+        return false;
     }
 }
