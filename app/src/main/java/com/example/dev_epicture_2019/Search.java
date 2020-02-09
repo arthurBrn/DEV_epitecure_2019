485989f2b17169d62dc48f5b5c6bdebdb1e45181
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-02-09 19:48:27 +0100

    [ADD] Search

--- app/src/main/java/com/example/dev_epicture_2019/Search.java
+++ app/src/main/java/com/example/dev_epicture_2019/Search.java
@@ -1,11 +1,49 @@
 package com.example.dev_epicture_2019;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Search extends Common {
 
+    private OkHttpClient httpClient;
+    private SearchView sv;
+
+    private static class PhotoVH extends RecyclerView.ViewHolder {
+        ImageView photo;
+        TextView title;
+        ImageButton favBtn;
+        int is_fav = 0;
+
+        public PhotoVH(View itemView) {
+            super(itemView);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +52,117 @@ public class Search extends Common {
         BottomNavigationView navigationBar = findViewById(R.id.navigationBar);
         Common.changeActivity(navigationBar, 1, getApplicationContext());
         overridePendingTransition(0, 0);
+        sv = findViewById(R.id.searchBarId);
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                fetchData(query);
+                return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
     }
+
+    private void fetchData(String search) {
+        httpClient = new OkHttpClient.Builder().build();
+        Request request = new Request.Builder()
+                .url("https://api.imgur.com/3/gallery/search?q=" + search)
+                .method("GET", null)
+                .header("Authorization", "Bearer " + accesToken)
+                .build();
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                try {
+                    final List<Photo> photos = new Parser().getData(response);
+                    runOnUiThread(() -> render(photos));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void render(final List<Photo> photos) {
+        RecyclerView rv = findViewById(R.id.recyclerView);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.Adapter<Search.PhotoVH> adapter = new RecyclerView.Adapter<Search.PhotoVH>() {
+            @NonNull
+            @Override
+            public Search.PhotoVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                Search.PhotoVH vh = new Search.PhotoVH(getLayoutInflater().inflate(R.layout.card, null));
+                vh.photo = vh.itemView.findViewById(R.id.image);
+                vh.title = vh.itemView.findViewById(R.id.title);
+                vh.favBtn = vh.itemView.findViewById(R.id.favBtn);
+                return vh;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull Search.PhotoVH holder, int position) {
+                String path = "https://i.imgur.com/" + photos.get(position).link + ".jpg";
+                Picasso.get().load(path).into(holder.photo);
+                holder.title.setText(photos.get(position).title);
+                String name = photos.get(position).title;
+                Boolean fav = photos.get(position).favorite;
+                String debug = name + fav;
+                Log.d("TAG", "onBindViewHolder: " + debug);
+                if (photos.get(position).favorite == true) {
+                    holder.is_fav = 1;
+                    checkHeart(holder);
+                } else {
+                    holder.is_fav = 0;
+                    uncheckHeart(holder);
+                }
+                holder.photo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Details.class);
+                        intent.putExtra("galleryId", photos.get(position).id);
+                        intent.putExtra("photo", photos.get(position).type);
+                        intent.putExtra("accessToken", getAccesToken());
+                        startActivity(intent);
+                    }
+                });
+                holder.favBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (holder.is_fav == 0) {
+                            checkHeart(holder);
+                            new Home().addAFavorite(photos.get(position).id, photos.get(position).type);
+                        } else if (holder.is_fav == 1) {
+                            uncheckHeart(holder);
+                            new Home().addAFavorite(photos.get(position).id, photos.get(position).type);
+                        }
+                    }
+                });
+            }
+            @Override
+            public int getItemCount() {
+                return photos.size();
+            }
+        };
+        rv.setAdapter(adapter);
+    }
+    public void checkHeart(@NonNull Search.PhotoVH holder) {
+        holder.favBtn.setImageResource(R.drawable.ic_favorite_black_24dp);
+        holder.is_fav = 1;
+    }
+
+    public void uncheckHeart(@NonNull Search.PhotoVH holder) {
+        holder.favBtn.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+        holder.is_fav = 0;
+    }
+
 }
